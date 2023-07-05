66823a9699fd5a57563ff567bccd4d23168e8036
Martin <joewwi03@gmail.com>
2023-07-05 16:52:57 +0300

    Add log-in and sign-up routes

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  resources :users, only: [:show]
+  get "/me", to: "users#show"
+  post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+end
