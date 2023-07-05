ccdea3dd341e60afafbb8c5f2ec63ded156533bb
Martin <joewwi03@gmail.com>
2023-07-05 18:01:27 +0300

    Add Auth

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   get "/me", to: "users#show"
   post "/signup", to: "users#create"
+
+  get "/auth", to: "users#show"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 end
