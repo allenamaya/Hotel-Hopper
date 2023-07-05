288b5519eb778ae30897ddff0d98f05dfeb37a43
Martin <joewwi03@gmail.com>
2023-07-05 18:27:55 +0300

    Debugged

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,16 @@
 class UsersController < ApplicationController
     wrap_parameters format: []
     skip_before_action :authourized, only: :create
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+    rescue_from ActiveRecord::RecordNotFound, with: :render_user_not_found
     
     def show
-        user = User.find_by(username: params[:username])
+        user = User.find(session[:user_id])
         render json: user, status: :ok
     end
 
     def create
-        user = User.create(user_params)
+        user = User.create!(user_params)
         if user.valid?
         render json: user, status: :created
         else
@@ -17,6 +19,14 @@ class UsersController < ApplicationController
     end
 
     private
+
+    def render_unprocessable_entity(invalid)
+        render json: {error: invalid.record.errors}, status: :unprocessable_entity
+    end
+
+    def render_user_not_found
+        render json: {error: "User not found"}, status: :not_found
+    end
 
     def user_params
         params.permit(:username, :password, :password_confirmation, :email, :in_mailing_list, :terms_and_services)
