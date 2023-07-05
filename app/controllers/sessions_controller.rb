2255ddbec6098509cfd9f07737efd3845e3af341
Martin <joewwi03@gmail.com>
2023-07-05 16:54:52 +0300

    Add log-in methods

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+    skip_before_action :authourized, only: :create
+
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+        session[:user_id] = user.id
+        render json: user, status: :created
+        else
+        render json: { error: "Invalid username or password" }, status: :unauthorized
+        end
+    end  
+
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+    
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation, :email)
+    end
+end
