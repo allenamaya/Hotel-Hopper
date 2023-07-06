e09b2b2b156f25b072e97aa1233d233dc07d40db
John Kipruto <johnkipruto157@gmail.com>
2023-07-06 22:26:14 +0300

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :rooms, only: [:index, :show, :update]
-  resources :customers, only: [:index, :show, :create]
+  resources :customers, only: [:index, :show, :create, :destroy, :update]
   resources :hotels, only: [:index, :show]
 
   get "/occupied", to: "rooms#occupied"
   get "/unoccupied", to: "rooms#unoccupied"
-
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
